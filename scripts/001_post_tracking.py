2a4d9a08e90c371b0f9fef0add5d42ed2d1342b5
degoldschmidt <goldschmidt.de@gmail.com>
2017-12-14 19:03:38 +0000

    neewwww

--- scripts/001_post_tracking.py
+++ scripts/001_post_tracking.py
@@ -1,12 +1,72 @@
+import os
+
 from pytrack_analysis import Multibench
 from pytrack_analysis.cli import get_args
+from pytrack_analysis.dataio import get_data
 from pytrack_analysis.profile import get_profile, get_scriptname, show_profile
+from pytrack_analysis.posttracking import get_files, get_frame_skips, get_session_list
 
 def main():
-    args = get_args(['exp', 'exp', 'Select experiment by four-letter ID'], ['u', 'user', 'Select user'], SILENT=False)
+    args = get_args(    ['exp', 'exp', 'Select experiment by four-letter ID'],
+                        ['from', 'sfrom', 'Select session from where to start analyzing'],
+                        ['not', 'snot', 'Select session which not to analyze'],
+                        ['only', 'sonly', 'Select session which only to analyze'],
+                        ['to', 'sto', 'Select session where to end analyzing'],
+                        ['u', 'user', 'Select user'],
+                        SILENT=True)
     profile = get_profile(args.exp, args.user, script=get_scriptname(__file__))
-    #show_profile(profile)
-    print(profile)
+    folders = profile.get_folders()
+    raw = folders['raw']
+    videos = folders['videos']
+
+    ### go through SESSIONS
+    for each_session in get_session_list(profile.Nvids(), args.sfrom, args.sto, args.snot, args.sonly):
+        ### get timestamp and all files from session folder
+        allfiles, dtstamp, timestr = get_files(raw, each_session, videos)
+
+        ### load raw data
+        raw_data, data_units = get_data(allfiles['data'])
+
+        ### analyze frameskips
+        skips, bskips, maxskip, max_skip_arg = get_frame_skips(raw_data, println=True)
+
+        ### detect arena geometry
+        arena = get_arena_geometry()
+
+        ### detect food spots
+        food_spots = get_food_spots()
+
+        ### translate to start position
+
+        ### translate trajectories ot arena center
+
+        ### scale trajectories to mm
+
+        ### detect mistracked frames
+
+        ### detect jumps
+
+        ### detect correct head positions
+
+        ### analyze general statistics from trajectory
+
+        ### save fly data and metadata files
+
+        """
+        #f, ax = plot_overlay(allfiles['video'], arenas=geom_data, spots=food_data)
+        #plot_along(f, ax)
+
+        ### getting metadata
+        meta = get_meta(allfiles, dtstamp, manual)
+        meta['datadir'] = session_folder.split('/')[-2:]
+        meta['experiment'] = EXP_ID
+        meta['num_frames'] = get_num_frames(raw_data)
+        for each_condition in meta['variables']:
+            for each_file in meta["files"]:
+                if each_condition in each_file:
+                    conditions = pd.read_csv(each_file, sep='\t', index_col='ix')
+        """
+        print()
 
 if __name__ == '__main__':
     # runs as benchmark test
--- scripts/001_post_tracking_old.py
+++ scripts/001_post_tracking_old.py
@@ -67,25 +67,7 @@ def get_disconts(data, thr_disc=80., thr_coh=0.001):        ### TODO: explicit!!
                     data.loc[index, 'flags'] = 1
     return data
 
-"""
-Returns dictionary of all raw data files (DATAIO)
-"""
-def get_files(timestampstring, basedir, video, noVideo=False):
-    if noVideo:
-        return {
-                        "data" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "fly" in eachfile and timestampstring in eachfile],
-                        "food" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "food" in eachfile and timestampstring in eachfile],
-                        "geometry" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "geometry" in eachfile and timestampstring in eachfile][0],
-                        "timestart" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "timestart" in eachfile and timestampstring in eachfile][0],
-        }
-    else:
-        return {
-                    "data" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "fly" in eachfile and timestampstring in eachfile],
-                    "food" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "food" in eachfile and timestampstring in eachfile],
-                    "geometry" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "geometry" in eachfile and timestampstring in eachfile][0],
-                    "timestart" : [os.path.join(basedir, eachfile) for eachfile in os.listdir(basedir) if "timestart" in eachfile and timestampstring in eachfile][0],
-                    "video" : [os.path.join(video, eachfile) for eachfile in os.listdir(video) if timestampstring in eachfile][0],
-                    }
+
 
 """
 Returns food spots data (DATAIO)
@@ -113,20 +95,7 @@ def get_frame_dims(filename):
     warnings.filterwarnings("default")
     return dims
 
-"""
-Returns number of frame skips, big frame skips (more than 10 frames) and maximum skipped time between frames (DATAIO)
-"""
-def get_frame_skips(data, println=False, skips='frame_dt'):
-    frameskips = data[skips]
-    max_skip = np.amax(frameskips)
-    max_skip_arg = frameskips.idxmax()
-    total = frameskips.shape[0]
-    strict_skips = np.sum(frameskips > (1/30)+(1/30))
-    easy_skips = np.sum(frameskips > (1/30)+(1/3))
-    if println:
-        print('Skips of more than 1 frames (>{:1.3f} s): {:} ({:3.3f}% of all frames)'.format((1/30)+(1/30), strict_skips, 100*strict_skips/total))
-        print('Skips of more than 10 frames (>{:1.3f} s): {:} ({:3.3f}% of all frames)'.format((1/30)+(1/3), easy_skips, 100*easy_skips/total))
-    return strict_skips, easy_skips, max_skip, max_skip_arg
+
 
 """
 Returns list of raw data for filenames (DATAIO)
@@ -337,17 +306,7 @@ def get_sign(image, frame, x, y, major, orird):
     pixeldiff = tailpx - headpx
     return np.sign(pixeldiff)
 
-"""
-Returns timestamp from session folder (DATAIO)
-"""
-def get_time(session):
-    from datetime import datetime
-    for each in os.listdir(session):
-        if "timestart" in each:
-            any_file = each
-    timestampstr = any_file.split('.')[0][-19:]
-    dtstamp = datetime.strptime(timestampstr, "%Y-%m-%dT%H_%M_%S")
-    return dtstamp, timestampstr[:-3]
+
 
 
 
