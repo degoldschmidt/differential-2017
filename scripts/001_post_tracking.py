9acc205b6989c155186a49c9ffcd30fe1c12212f
degoldschmidt <goldschmidt.de@gmail.com>
2018-01-03 10:12:17 +0100

    noVideo option

--- scripts/001_post_tracking.py
+++ scripts/001_post_tracking.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 
 from pytrack_analysis import Multibench
 from pytrack_analysis.cli import get_args
@@ -15,9 +16,14 @@ def main():
                         ['only', 'sonly', 'Select session which only to analyze'],
                         ['to', 'sto', 'Select session where to end analyzing'],
                         ['u', 'user', 'Select user']]
-    opt_args = [['plot', 'Option whether to plot']]
-    args = get_args(var_args, opt_args,SILENT=True)
+    opt_args = [['load', 'Option whether to load file'],
+                ['novideo', 'Option whether to use videos'],
+                ['plot', 'Option whether to plot']]
+    args = get_args(var_args, opt_args, SILENT=True)
     profile = get_profile(args.exp, args.user, script=get_scriptname(__file__))
+    load_from_file = args.load
+    if args.novideo:
+        load_from_file = True
     folders = profile.get_folders()
 
     ### go through video sessions
@@ -26,7 +32,7 @@ def main():
         ### load video session data and metadata
         colnames = ['datetime', 'elapsed_time', 'frame_dt', 'body_x',   'body_y',   'angle',    'major',    'minor']
         colunits = ['Datetime', 's',            's',        'px',       'px',       'rad',      'px',       'px']
-        session_data = RawData(args.exp, each_session, folders, columns=colnames, units=colunits)
+        session_data = RawData(args.exp, each_session, folders, columns=colnames, units=colunits, noVideo=args.novideo)
         ### scale trajectories to mm
         scale = 8.543
         session_data.set_scale('fix_scale', scale, unit='mm')
@@ -44,24 +50,30 @@ def main():
             each_df['tail_x'] = scale * head_tails[2] + session_data.arenas[i_arena].x
             each_df['tail_y'] = scale * head_tails[3] + session_data.arenas[i_arena].y
             ### detect head flips
-            flip, headpx, tailpx = get_pixel_flip(each_df,
-                                                hx='head_x', hy='head_y', tx='tail_x', ty='tail_y',
-                                                video=session_data.video_file,
-                                                start=session_data.first_frame)
-            each_df['flip'] = flip
-            each_df['headpx'] = headpx
-            each_df['tailpx'] = tailpx
-            out_id = 4 * (each_session-1) + i_arena + 1
-            outfile = os.path.join(folders['processed'],'post_tracking','{}_{:03d}.csv'.format(args.exp, out_id))
-            print("Saving data to", outfile)
-            each_df.to_csv(outfile, index_label='frame')
+            file_id = 4 * (each_session-1) + i_arena + 1
+            _file = os.path.join(folders['processed'],'post_tracking','{}_{:03d}.csv'.format(args.exp, file_id))
+            if load_from_file:
+                print("Loading data from", _file)
+                df = pd.read_csv(_file, index_col='frame')
+                each_df['flip'] = df['flip']
+                each_df['headpx'] = df['headpx']
+                each_df['tailpx'] = df['tailpx']
+            else:
+                flip, headpx, tailpx = get_pixel_flip(each_df, hx='head_x', hy='head_y', tx='tail_x', ty='tail_y', video=session_data.video_file, start=session_data.first_frame)
+                each_df['flip'] = flip
+                each_df['headpx'] = headpx
+                each_df['tailpx'] = tailpx
+                print("Saving data to", _file)
+                each_df.to_csv(_file, index_label='frame')
+
+
 
         if args.plot:
-            f, ax = plot_fly(session_data.raw_data[0], x='body_x', y='body_y', hx='head_x', hy='head_y')
-            plot_along(f, ax)
-            #for i in range(4):
-                #f, ax = plot_ts(session_data.raw_data[i], x='frame', y=['frame_dt', 'angle', 'major', 'minor', 'displacement'], units=['s', 'rad', 'mm', 'mm', 'mm/frame'])
-                #plot_along(f, ax)
+            #f, ax = plot_fly(session_data.raw_data[0], x='body_x', y='body_y', hx='head_x', hy='head_y')
+            #plot_along(f, ax)
+            for i in range(4):
+                f, ax = plot_ts(session_data.raw_data[i], x='frame', y=['frame_dt', 'angle', 'major', 'minor', 'displacement', 'flip'], units=['s', 'rad', 'mm', 'mm', 'mm/frame', ''])
+                plot_along(f, ax)
 
         ### detect jumps
 
