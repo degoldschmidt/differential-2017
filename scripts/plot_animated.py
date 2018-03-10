cb1dbd9ddb357c3d66a513279c764ba2e2318e80
degoldschmidt <goldschmidt.de@gmail.com>
2018-03-10 18:34:14 +0000

    just ani

--- scripts/plot_animated.py
+++ scripts/plot_animated.py
@@ -33,22 +33,6 @@ def get_tseries_axes(grid, position, sharex=None):
     line, = ax.plot([], [])
     return ax, line
 
-# animate time series
-def animate(frame, *args):
-    lines = args[0]
-    xarray = args[1]
-    yarrays = args[2]
-    xdata = np.array(xarray.loc[:frame+1])
-    ydata = [None for i in range(5)]
-    for j, each in enumerate(yarrays.columns):
-        ydata[j] = np.array(yarrays.loc[:frame+1, each])
-    # update the data of both line objects
-    for j,each_line in enumerate(lines):
-        each_line.set_data(xdata, ydata[j])
-    return lines
-
-# animate video
-radius = int(8.543 * 12.5)
 palette = {    -1: '#ff00fc',
                 0: '#ff00c7',
                 1: '#c97aaa',
@@ -57,6 +41,41 @@ palette = {    -1: '#ff00fc',
                 4: '#ff7f00',
                 5: '#1f78b4',
                 6: '#ff1100'}
+palette2 ={     0: '#ffffff',
+                1: '#ff7f00',
+                2: '#1f78b4'}
+# animate time series
+def animate(frame, *args):
+    lines = args[0]
+    xarray = args[1]
+    yarrays = args[2]
+    ax_ts = args[3]
+    xdata = np.array(xarray.loc[:frame+1])
+    ydata = [None for i in range(5)]
+    for j, each in enumerate(yarrays.columns):
+        if j < 2:
+            ydata[j] = int(yarrays.loc[frame, each])
+        else:
+            ydata[j] = np.array(yarrays.loc[:frame+1, each])
+    # update the data of both line objects
+    for j,each_line in enumerate(lines):
+        if j < 2:
+            if j == 1:
+                ax_ts[j].vlines(xarray.loc[frame], 0, 1, color=palette[ydata[j]])
+            if j == 0:
+                ax_ts[j].vlines(xarray.loc[frame], 0, 1, color=palette2[ydata[j]])
+                if frame-1 in xarray.index:
+                    if xarray.loc[frame] - xarray.loc[frame-1] > 0.1:
+                        a = xarray.loc[frame]-0.0333
+                        while a > xarray.loc[frame-1]:
+                            ax_ts[j].vlines(a, 0, 1, color=palette2[ydata[j]])
+                            a -= 0.0333
+        else:
+            each_line.set_data(xdata, ydata[j])
+    return lines
+
+# animate video
+radius = int(8.543 * 12.5)
 print(radius) ##100
 def updatefig(i, *image):
     xpos, ypos = image[3][0][i-4970], image[3][1][i-4970]
@@ -91,8 +110,8 @@ def init_animation(data, time=None, cols=None, video=None, figsize=(8,6), interv
         T = np.array(data.index)
     else:
         T = np.arange(interval[0],interval[1],dtype=np.int32)
-    xarray = df.loc[T[0]:T[-1]-1, time] - df.loc[T[0], time]
-    yarrays = df.loc[T[0]:T[-1]-1, cols]
+    xarray = df.loc[T[0]:T[-1], time] - df.loc[T[0], time]
+    yarrays = df.loc[T[0]:T[-1], cols]
     gs = GridSpec(N, 2*N, height_ratios=[2,2,2,1,1])
     gs.update(wspace=2)
     ## video axis
@@ -100,8 +119,8 @@ def init_animation(data, time=None, cols=None, video=None, figsize=(8,6), interv
     ax_video = plt.subplot(gs[:,:N]) ### N == 5
     ax_video.set_aspect('equal')
     sns.despine(ax=ax_video, bottom=True, left=True)
-    #ax_video.get_xaxis().set_visible(False)
-    #ax_video.get_yaxis().set_visible(False)
+    ax_video.get_xaxis().set_visible(False)
+    ax_video.get_yaxis().set_visible(False)
     # initial frame
     radius = int(8.543 * 12.5)
     y = int(359.3853 - 8.543 * (-1.))##image[3][1][i-6101]
@@ -135,18 +154,25 @@ def init_animation(data, time=None, cols=None, video=None, figsize=(8,6), interv
         lines.append(l)
     return fig, ax_video, ax_tseries, T, xarray, yarrays, im, vid, lines
 
-def run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, pixelpos, factor=1, outfile="out.mp4"):
+def run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, ax_ts, pixelpos, factor=1, outfile="out.mp4"):
     myinterval = 1000./(30*factor)
     print("Interval between frame: {}".format(myinterval))
-    ani_lines = anim.FuncAnimation(fig, animate, frames, blit=True, fargs=(lines, xarray, yarrays), interval=myinterval)
+    ani_lines = anim.FuncAnimation(fig, animate, frames, blit=True, fargs=(lines, xarray, yarrays, ax_ts), interval=myinterval)
     ani_image = anim.FuncAnimation(fig, updatefig, frames, blit=True, fargs=(im, vid, ax_video, pixelpos), interval=myinterval)
     #plt.tight_layout()
     #ani_image.save(outfile, writer='ffmpeg', dpi=300)
     ani_lines.save(outfile, extra_anim=[ani_image], writer='ffmpeg', dpi=300)
 
-def respine(ax, interval, bottom):
-    ax.set_ylim(interval)
-    sns.despine(ax=ax, bottom=bottom, trim=True)
+def respine(ax, interval, tickint, bottom):
+    if interval is None:
+        ax.set_ylim([0, 1/tickint])
+        ax.set_yticks([])
+        ax.spines['left'].set_visible(False)
+        sns.despine(ax=ax, left=True, bottom=bottom, trim=True)
+    else:
+        ax.set_yticks(np.arange(interval[0], interval[1]+1,tickint))
+        ax.set_ylim(interval)
+        sns.despine(ax=ax, bottom=bottom, trim=True)
     return ax
 
 if __name__ == '__main__':
@@ -165,16 +191,6 @@ if __name__ == '__main__':
     profile = get_profile(experiment, 'degoldschmidt', script=thisscript)
 
     ### input data
-    """
-    df = pd.DataFrame({ 'time': 5.*np.arange(T),
-                        'speed': np.random.rand(T),
-                        'angular speed': np.random.rand(T),
-                        'distance': np.random.rand(T),
-                        'more stuff': np.random.rand(T),
-                        'even more': np.random.rand(T),
-                       })
-    data = [col for col in df.columns if not col == 'time']
-    """
     _in, _in2 = 'kinematics', 'classifier'
     _out = 'plots'
     infolder = os.path.join(profile.out(), _in)
@@ -190,23 +206,24 @@ if __name__ == '__main__':
     csv_file2 = os.path.join(infolder2,  '{}_{}.csv'.format(session.name, _in2))
     kinedf = pd.read_csv(csv_file, index_col='frame')
     ethodf = pd.read_csv(csv_file2, index_col='frame')
-    df = pd.concat([kinedf[['elapsed_time', 'sm_head_speed', 'angular_speed', 'dcenter']], ethodf[['etho', 'visit']]], axis=1)
-    data_cols = ['visit', 'etho', 'angular_speed', 'sm_head_speed', 'dcenter']
+    kinedf['min_patch'] = kinedf.loc[:,['dpatch_{}'.format(i) for i in range(11)]].min(axis=1)
+    df = pd.concat([kinedf[['elapsed_time', 'sm_head_speed', 'angular_speed', 'min_patch']], ethodf[['etho', 'visit']]], axis=1)
+    data_cols = ['visit', 'etho', 'angular_speed', 'sm_head_speed', 'min_patch']
     fig, ax_video, ax_tseries, frames, xarray, yarrays, im, vid, lines = init_animation(df, time='elapsed_time', cols=data_cols, video=video_file, figsize=(16,6), interval=[START,END])
     lines[0].set_color('#222222')
     ax_tseries[0].set_xlabel('Time [s]')
 
+    ax_tseries[4] = respine(ax_tseries[4], [0,5], 1, True)
+    ax_tseries[3] = respine(ax_tseries[3], [0,8], 4, True)
+    ax_tseries[2] = respine(ax_tseries[2], [-600,600], 300, True)
+    ax_tseries[1] = respine(ax_tseries[1], None, 1, True)
+    ax_tseries[0] = respine(ax_tseries[0], None, 0.5, False)
+
     ax_tseries[4].set_ylabel('Min. distance\nto patch [mm]')
     ax_tseries[3].set_ylabel('Linear\nspeed [mm/s]')
     ax_tseries[2].set_ylabel('Angular\nspeed [º/s]')
-    ax_tseries[1].set_ylabel('Ethogram')
-    ax_tseries[0].set_ylabel('Visits')
-
-    ax_tseries[4] = respine(ax_tseries[4], [0,15], True)
-    ax_tseries[3] = respine(ax_tseries[3], [0,8], True)
-    ax_tseries[2] = respine(ax_tseries[2], [-600,600], True)
-    #ax_tseries[1] = respine(ax_tseries[1], [0,1], True)
-    #ax_tseries[0] = respine(ax_tseries[0], [0,1], False)
+    ax_tseries[1].set_ylabel('Ethogram', labelpad=40)
+    ax_tseries[0].set_ylabel('Visits', labelpad=40)
 
     pixelpos = [np.zeros(frames.shape), np.zeros(frames.shape), np.zeros(frames.shape), np.zeros(frames.shape), np.zeros(frames.shape)]
     scale, x0, y0 = meta['arena']['scale'], meta['arena']['x'], meta['arena']['y']
@@ -218,6 +235,6 @@ if __name__ == '__main__':
 
     ### save animation to file
     _file = os.path.join(outfolder, "{}_{}.mp4".format(_outfile, session.name))
-    run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, pixelpos, outfile=_file)
+    run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, ax_tseries, pixelpos, outfile=_file)
     ### delete objects
     del profile
