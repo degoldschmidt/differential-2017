606985b8f6df5372c514631ade5dd230cd7e843c
degoldschmidt <goldschmidt.de@gmail.com>
2018-03-11 03:59:53 +0000

    done with animations

--- scripts/plot_animated.py
+++ scripts/plot_animated.py
@@ -23,9 +23,19 @@ import argparse
 
 ONLY_VIDEO = False
 ONLY_TRAJ = True
-NO_ANNOS = True
+NO_ANNOS = False
 
-def get_tseries_axes(grid, position, sharex=None):
+def unflatten(l):
+  newlist = []
+  for each in l:
+    if type(each) is list:
+      for i in each:
+        newlist.append(i)
+    else:
+      newlist.append(each)
+  return newlist
+
+def get_tseries_axes(grid, position, sharex=None, n=1, labels=[]):
     row=position[0]
     col=position[1]
     if sharex is None:
@@ -33,8 +43,14 @@ def get_tseries_axes(grid, position, sharex=None):
     else:
         ax = plt.subplot(grid[row, col:])
         ax.xaxis.set_visible(False)
-    line, = ax.plot([], [])
-    return ax, line
+    if n == 1:
+        lines, = ax.plot([], [])
+    elif n > 1:
+        lines = []
+        for i in range(n):
+            line, = ax.plot([], [], label=labels[i])
+            lines.append(line)
+    return ax, lines
 
 palette = {    -1: '#ff00fc',
                 0: '#ff00c7',
@@ -53,13 +69,14 @@ def animate(frame, *args):
     xarray = args[1]
     yarrays = args[2]
     ax_ts = args[3]
+    cols = args[4]
     xdata = np.array(xarray.loc[:frame+1])
-    ydata = [None for i in range(5)]
-    for j, each in enumerate(yarrays.columns):
-        if j < 2:
-            ydata[j] = int(yarrays.loc[frame, each])
-        else:
-            ydata[j] = np.array(yarrays.loc[:frame+1, each])
+    ydata = [None for i in range(6)]
+    for j, each in enumerate(cols):
+      if j < 2:
+          ydata[j] = int(yarrays.loc[frame, each])
+      else:
+          ydata[j] = np.array(yarrays.loc[:frame+1, each])
     # update the data of both line objects
     for j,each_line in enumerate(lines):
         if j < 2:
@@ -74,13 +91,21 @@ def animate(frame, *args):
                             ax_ts[j].vlines(a, 0, 1, color=palette2[ydata[j]])
                             a -= 0.0333
         else:
-            each_line.set_data(xdata, ydata[j])
+          each_line.set_data(xdata, ydata[j])
     return lines
 
 # animate video
 radius = int(8.543 * 12.5)
 print(radius) ##100
 def updatefig(i, *image):
+    beh = {    -1: "unclassified",
+                0: "resting",
+                1: "micromovement",
+                2: "walking",
+                3: "sharp turn",
+                4: "yeast micromovement",
+                5: "sucrose micromovement",
+                6: "jump/mistrack"}
     xpos, ypos = image[3][0][i-4970], image[3][1][i-4970]
     etho = image[3][2][i-4970]
     bxpos, bypos = image[3][3][i-4970], image[3][4][i-4970]
@@ -94,22 +119,29 @@ def updatefig(i, *image):
     if len(image[2].get_lines()) == 0:
         image[2].plot([xpos-a, bxpos-a], [ypos-b, bypos-b], color='#ff228c', ls='-', lw=1)
     image[0].set_array(image[1].get_data(i)[y-radius:y+radius, x-radius:x+radius])
-    image[2].set_title("frame #{}".format(i))
-    if not NO_ANNOS:
-        image[2].plot(xpos-a, ypos-b, color=palette[etho], marker='.', markersize=5)
+    if NO_ANNOS:
+        image[2].set_title("frame #{}".format(i))
     else:
-        image[2].plot(xpos-a, ypos-b, color='#ff228c', marker='.', markersize=5)
+        image[2].set_title("frame #{}\n{}".format(i, beh[etho]))
+    if not NO_ANNOS:
+        image[2].plot(xpos-a, ypos-b, color=palette[etho], marker='.', markersize=4)
+        image[2].get_lines()[0].set_data([xpos-a, bxpos-a], [ypos-b, bypos-b])
+        image[2].get_lines()[0].set_color(palette[etho])
+    else:
+        image[2].plot(xpos-a, ypos-b, color='#ff228c', marker='.', markersize=4)
         image[2].get_lines()[0].set_data([xpos-a, bxpos-a], [ypos-b, bypos-b])
     return image[0],
 
-def init_animation(data, time=None, cols=None, video=None, figsize=(8,6), interval=None, playback=1, meta=None):
-    ts_colors = [   '#8dd3c7',
-                    '#fcfc2f',
-                    '#bebada',
+def init_animation(data, time=None, cols=None, video=None, figsize=(10.666,6), interval=None, playback=1, meta=None):
+    ts_colors = [   '#000000',
+                    '#000000',
                     '#fb8072',
-                    '#80b1d3',]
+                    '#1f5fd5',
+                    '#000000',
+                    '#000000',]
     spot_colors = {'yeast': '#ffc04c', 'sucrose': '#4c8bff'}
-    fig = plt.figure(figsize=figsize)
+    fig = plt.figure(figsize=figsize, dpi=180)
+    #fig.set_size_inches(1920/300., 1080/300., True)
     ## gridspec
     N = len(cols)   # number of rows
     if interval is None:
@@ -117,19 +149,25 @@ def init_animation(data, time=None, cols=None, video=None, figsize=(8,6), interv
     else:
         T = np.arange(interval[0],interval[1],dtype=np.int32)
     xarray = data.loc[T[0]:T[-1], time] - data.loc[T[0], time]
-    yarrays = data.loc[T[0]:T[-1], cols]
+    ucols = unflatten(cols)
+    print(ucols)
+    yarrays = data.loc[T[0]:T[-1], ucols]
     gs = GridSpec(N, 2*N, height_ratios=[2,2,2,1,1])
     gs.update(wspace=2)
     ## video axis
     vid = imageio.get_reader(video)
     if ONLY_VIDEO:
-        fig, ax_video = plt.subplots(figsize=(6,6))
+        fig, ax_video = plt.subplots(figsize=figsize, dpi=180)
+        #fig.set_size_inches(1920/300., 1080/300., True)
     else:
-        ax_video = plt.subplot(gs[:,:N]) ### N == 5
+        ax_video = plt.subplot(gs[:,:4]) ### N == 5
     ax_video.set_aspect('equal')
     sns.despine(ax=ax_video, bottom=True, left=True)
     ax_video.get_xaxis().set_visible(False)
     ax_video.get_yaxis().set_visible(False)
+    opos = ax_video.get_position()
+    if not ONLY_VIDEO:
+      ax_video.set_position([opos.x0-0.075, opos.y0, opos.width*1.25, opos.height*1.25])
     # initial frame
     scale = meta['arena']['scale']
     radius = int(scale * 12.5)
@@ -150,7 +188,9 @@ def init_animation(data, time=None, cols=None, video=None, figsize=(8,6), interv
     ax_tseries, lines = None, None
     if not ONLY_VIDEO:
         ax_tseries, lines = [], []
-        ax, l = get_tseries_axes(gs, [4, 5])
+        ax, l = get_tseries_axes(gs, [4, 4])
+        opos = ax.get_position()
+        ax.set_position([opos.x0+0.025, opos.y0, opos.width*1.05, opos.height])
         l.set_color(ts_colors[0])
         ax.set_xlim(0, 1.1*np.max(xarray))
         if np.min(data[cols[-1]]) < 0:
@@ -162,42 +202,52 @@ def init_animation(data, time=None, cols=None, video=None, figsize=(8,6), interv
         ax.set_ylabel(cols[-1])
         ax_tseries.append(ax)
         lines.append(l)
+        count = 0
         for i in range(1,5):
-            ax, l = get_tseries_axes(gs, [4-i, 5], sharex=ax_tseries[0])
-            l.set_color(ts_colors[i])
-            ax.set_xlim(0, 1.1*np.max(xarray))
-            ax.set_ylabel(cols[-i-1])
-            if np.min(data[cols[-i-1]]) < 0:
-                ax.set_ylim(1.1*np.min(data[cols[-i-1]]), 1.1*np.max(data[cols[-i-1]]))
+            if type(cols[i]) is list:
+              ax, l = get_tseries_axes(gs, [4-i, 4], sharex=ax_tseries[0], n=2, labels=['head', 'body'])
             else:
-                ax.set_ylim(0, 1.1*np.max(data[cols[-i-1]]))
-            sns.despine(ax=ax, bottom=True, trim=True)
+              ax, l = get_tseries_axes(gs, [4-i, 4], sharex=ax_tseries[0])
+            opos = ax.get_position()
+            ax.set_position([opos.x0+0.025, opos.y0, opos.width*1.05, opos.height])
+            if type(l) is list:
+              for jj,each in enumerate(l):
+                each.set_color(ts_colors[count])
+                lines.append(each)
+                ax.legend()
+                count += 1
+            else:
+              l.set_color(ts_colors[count])
+              lines.append(l)
+              count += 1
             ax_tseries.append(ax)
-            lines.append(l)
+
+        print(len(ax_tseries), len(lines))
 
         ### thresholds
-        ax_tseries[4].hlines(2.5, ax_tseries[4].get_xlim()[0], ax_tseries[4].get_xlim()[1], colors="#aaaaaa", linestyles='dashed')
-        ax_tseries[4].hlines(5, ax_tseries[4].get_xlim()[0], ax_tseries[4].get_xlim()[1], colors="#aaaaaa", linestyles='dashed')
+        if not NO_ANNOS:
+            print('Thresholds drawn')
+            ax_tseries[4].hlines(2.5, 0, 30, colors="#aaaaaa", linestyles='dashed')
+            ax_tseries[4].hlines(5, 0, 30, colors="#aaaaaa", linestyles='dashed')
+            ax_tseries[3].hlines(2., 0, 30, colors="#aaaaaa", linestyles='dashed')
+            #ax_tseries[3].hlines(4., 0, 30, colors="#aaaaaa", linestyles='dashed')
+            ax_tseries[2].hlines(125., 0, 30, colors="#aaaaaa", linestyles='dashed')
+            ax_tseries[2].hlines(-125., 0, 30, colors="#aaaaaa", linestyles='dashed')
 
-        ax_tseries[3].hlines(2., ax_tseries[4].get_xlim()[0], ax_tseries[4].get_xlim()[1], colors="#aaaaaa", linestyles='dashed')
-        #ax_tseries[3].hlines(4., ax_tseries[4].get_xlim()[0], ax_tseries[4].get_xlim()[1], colors="#aaaaaa", linestyles='dashed')
+    return fig, ax_video, ax_tseries, T, xarray, yarrays, im, vid, lines, ucols
 
-        ax_tseries[2].hlines(125., ax_tseries[4].get_xlim()[0], ax_tseries[4].get_xlim()[1], colors="#aaaaaa", linestyles='dashed')
-        ax_tseries[2].hlines(-125., ax_tseries[4].get_xlim()[0], ax_tseries[4].get_xlim()[1], colors="#aaaaaa", linestyles='dashed')
-
-    return fig, ax_video, ax_tseries, T, xarray, yarrays, im, vid, lines
-
-def run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, ax_ts, pixelpos, factor=1, outfile="out.mp4"):
+def run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, ax_ts, pixelpos, factor=1, cols=None, outfile="out.mp4"):
     myinterval = 1000./(30*factor)
     print("Interval between frame: {}".format(myinterval))
     if not ONLY_VIDEO:
-        ani_lines = anim.FuncAnimation(fig, animate, frames, blit=True, fargs=(lines, xarray, yarrays, ax_ts), interval=myinterval)
+        ani_lines = anim.FuncAnimation(fig, animate, frames, blit=True, fargs=(lines, xarray, yarrays, ax_ts, cols), interval=myinterval)
     ani_image = anim.FuncAnimation(fig, updatefig, frames, blit=True, fargs=(im, vid, ax_video, pixelpos), interval=myinterval)
     #plt.tight_layout()
+    print(fig.get_size_inches()*fig.dpi)
     if ONLY_VIDEO:
-        ani_image.save(outfile, writer='ffmpeg', dpi=300)
+        ani_image.save(outfile, writer='ffmpeg', dpi=180)
     else:
-        ani_lines.save(outfile, extra_anim=[ani_image], writer='ffmpeg', dpi=300)
+        ani_lines.save(outfile, extra_anim=[ani_image], writer='ffmpeg', dpi=180)
 
 def respine(ax, interval, tickint, bottom):
     if interval is None:
@@ -251,15 +301,26 @@ def main():
     kinedf = pd.read_csv(csv_file, index_col='frame')
     ethodf = pd.read_csv(csv_file2, index_col='frame')
     kinedf['min_patch'] = kinedf.loc[:,['dpatch_{}'.format(i) for i in range(11)]].min(axis=1)
-    df = pd.concat([kinedf[['elapsed_time', 'sm_head_speed', 'angular_speed', 'min_patch']], ethodf[['etho', 'visit']]], axis=1)
-    data_cols = ['visit', 'etho', 'angular_speed', 'sm_head_speed', 'min_patch']
-    fig, ax_video, ax_tseries, frames, xarray, yarrays, im, vid, lines = init_animation(df, time='elapsed_time', cols=data_cols, video=video_file, figsize=(16,6), interval=[START,END], meta=meta)
+    df = pd.concat([kinedf[['elapsed_time', 'sm_head_speed', 'sm_body_speed', 'angular_speed', 'min_patch']], ethodf[['etho', 'visit']]], axis=1)
+    data_cols = ['visit', 'etho', 'angular_speed', ['sm_head_speed', 'sm_body_speed'], 'min_patch']
+    fig, ax_video, ax_tseries, frames, xarray, yarrays, im, vid, lines, ucols = init_animation(df, time='elapsed_time', cols=data_cols, video=video_file, figsize=(8,6), interval=[START,END], meta=meta) ##(10.6666666,6)
     if not ONLY_VIDEO:
         lines[0].set_color('#222222')
         ax_tseries[0].set_xlabel('Time [s]')
+        ax_tseries[4].set_xlim([0,30])
+        ax_tseries[3].set_xlim([0,30])
+        ax_tseries[2].set_xlim([0,30])
+        ax_tseries[1].set_xlim([0,30])
+        ax_tseries[0].set_xlim([0,30])
         ax_tseries[4] = respine(ax_tseries[4], [0,10], 2.5, True)
         ax_tseries[3] = respine(ax_tseries[3], [0,20], [0,2,5,10,15], True)
-        ax_tseries[2] = respine(ax_tseries[2], [-600,600], [-500,-125,0,125,500], True)
+        if NO_ANNOS:
+            ax_tseries[2] = respine(ax_tseries[2], [-600,600], [-500,-125,0,125,500], False)
+            ax_tseries[2].get_xaxis().set_visible(True)
+            ax_tseries[2].set_xticks(np.arange(0,30+1,5))
+            ax_tseries[2].set_xlabel('Time [s]')
+        else:
+            ax_tseries[2] = respine(ax_tseries[2], [-600,600], [-500,-125,0,125,500], True)
         ax_tseries[1] = respine(ax_tseries[1], None, 1, True)
         ax_tseries[0] = respine(ax_tseries[0], None, 0.5, False)
         ax_tseries[4].set_ylabel('Min. distance\nto patch [mm]')
@@ -267,9 +328,9 @@ def main():
         ax_tseries[2].set_ylabel('Angular\nspeed [º/s]')
         ax_tseries[1].set_ylabel('Ethogram', labelpad=40)
         ax_tseries[0].set_ylabel('Visits', labelpad=40)
-    if NO_ANNOS:
-        ax_tseries[1].set_visible(False)
-        ax_tseries[0].set_visible(False)
+        if NO_ANNOS:
+          ax_tseries[1].set_visible(False)
+          ax_tseries[0].set_visible(False)
 
     pixelpos = [np.zeros(frames.shape), np.zeros(frames.shape), np.zeros(frames.shape), np.zeros(frames.shape), np.zeros(frames.shape)]
     scale, x0, y0 = meta['arena']['scale'], meta['arena']['x'], meta['arena']['y']
@@ -281,7 +342,7 @@ def main():
 
     ### save animation to file
     _file = os.path.join(outfolder, "{}_{}.mp4".format(_outfile, session.name))
-    run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, ax_tseries, pixelpos, outfile=_file)
+    run_animation(fig, frames, xarray, yarrays, lines, im, vid, ax_video, ax_tseries, pixelpos, cols=ucols, outfile=_file)
     ### delete objects
     del profile
 
